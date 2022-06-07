41ac35961ea384a24c68afa4282d44c3a28ac71e
ccrismota <ccrismota@gmail.com>
2022-06-07 18:46:09 -0300

    PR 1- listagem por id-cliente

---
+++ src/main/java/com/api/helpr/services/ClienteService.java
@@ -0,0 +1,42 @@
+package com.api.helpr.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.api.helpr.domain.Cliente;
+import com.api.helpr.domain.Pessoa;
+import com.api.helpr.domain.dtos.ClienteDTO;
+import com.api.helpr.repositories.ClienteRepository;
+import com.api.helpr.repositories.PessoaRepository;
+import com.api.helpr.services.exceptions.DataIntegrityViolationException;
+import com.api.helpr.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repository;
+	
+	@Autowired
+	private PessoaRepository pessoaRepository;
+
+	public Cliente findById(Integer id) {
+		Optional<Cliente> obj = repository.findById(id);
+		return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado" +id));
+	}
+	
+	private void validaCpfEEmail(ClienteDTO objDto) {
+
+		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDto.getCpf());
+		if (obj.isPresent() && obj.get().getId() != objDto.getId()) {
+			throw new DataIntegrityViolationException("CPF já cadastrado no sistema!");
+		}
+
+		obj = pessoaRepository.findByEmail(objDto.getEmail());
+		if (obj.isPresent() && obj.get().getId() != objDto.getId()) {
+			throw new DataIntegrityViolationException("E-mail já cadastrado no sistema!");
+		}
+	}
+}
