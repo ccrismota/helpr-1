92fe38f53e81e75442703d75f5d88761a69e813a
ccrismota <ccrismota@gmail.com>
2022-06-08 15:56:39 -0300

    POST de Adição e serviço   (PR3)

--- src/main/java/com/api/helpr/domain/Cliente.java
+++ src/main/java/com/api/helpr/domain/Cliente.java
@@ -2,10 +2,12 @@ package com.api.helpr.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
+import com.api.helpr.domain.dtos.ClienteDTO;
 import com.api.helpr.domain.enums.Perfil;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -27,6 +29,18 @@ public class Cliente extends Pessoa {
 		super(id, nome, cpf, email, senha);
 		addPerfils(Perfil.CLIENTE);
 	}
+	
+	public  Cliente( ClienteDTO obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.cpf = obj.getCpf();
+		this.email =obj.getEmail();
+		this.senha = obj.getSenha();
+		this.perfils = obj.getPerfils().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+	}
+	
+	
 
 	public List<Chamado> getChamados() {
 		return chamados;
--- src/main/java/com/api/helpr/resources/ClienteResource.java
+++ src/main/java/com/api/helpr/resources/ClienteResource.java
@@ -1,14 +1,20 @@
 package com.api.helpr.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.api.helpr.domain.Cliente;
 import com.api.helpr.domain.Tecnico;
@@ -35,6 +41,14 @@ public class ClienteResource {
 		List<ClienteDTO> listDto = list.stream()
 				.map(cli -> new ClienteDTO(cli)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
+	}
+	
+	@PostMapping
+	public ResponseEntity<ClienteDTO> createCliente(@Valid @RequestBody ClienteDTO objDto){
+		Cliente newObj = service.create(objDto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(newObj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 	
 }
